fdc7664bb59b547b15b1db84f27ea31f03cea7b8
Diana Monegro <diana2monegro1@gmail.com>
2022-09-07 09:26:36 -0400

    Adding Product entity

---
+++ src/entities/Product.java
@@ -0,0 +1,36 @@
+package entities;
+
+import enums.MeasureUnit;
+
+public class Product extends BaseEntity<Long>{
+    private String name;
+    private Double price;
+    private MeasureUnit measureUnit;
+
+    public Product() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public MeasureUnit getMeasureUnit() {
+        return measureUnit;
+    }
+
+    public void setMeasureUnit(MeasureUnit measureUnit) {
+        this.measureUnit = measureUnit;
+    }
+}
