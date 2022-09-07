717beb72a3d8e90ed28e01166eed5f177f349596
Diana Monegro <diana2monegro1@gmail.com>
2022-09-07 09:23:43 -0400

    Adding MeasurementUnit enum class

---
+++ src/enums/MeasurementUnit.java
@@ -0,0 +1,14 @@
+package enums;
+
+public enum MeasurementUnit {
+    LIBRAS,
+    KILOS,
+    MILIGRAMOS,
+    GRAMOS,
+    ONZAS,
+    BOTELLAS,
+    LATAS,
+    SACOS,
+    GALONES,
+    UNIDAD
+}
