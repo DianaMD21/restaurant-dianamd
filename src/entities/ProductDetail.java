fd6d6a8256010bb35bcc92404923c5a2d0f1d2be
Diana Monegro <diana2monegro1@gmail.com>
2022-09-10 12:52:44 -0400

    Adding ProductDetail Entity

---
+++ src/entities/ProductDetail.java
@@ -0,0 +1,56 @@
+package entities;
+
+import enums.MeasureUnit;
+
+import java.util.List;
+
+public class ProductDetail extends BaseEntity<Long>{
+    private Product product;
+    private String name;
+    private Double price;
+    private MeasureUnit measureUnit;
+    private List<Tax> taxes;
+
+    public ProductDetail() {
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public MeasureUnit getMeasureUnit() {
+        return measureUnit;
+    }
+
+    public void setMeasureUnit(MeasureUnit measureUnit) {
+        this.measureUnit = measureUnit;
+    }
+
+    public List<Tax> getTaxes() {
+        return taxes;
+    }
+
+    public void setTaxes(List<Tax> taxes) {
+        this.taxes = taxes;
+    }
+}
