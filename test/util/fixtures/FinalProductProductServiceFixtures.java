4d74103b595cca211f78600e5537a9cf834f45c8
DianaMD21 <diana2monegro1@gmail.com>
2023-01-10 10:41:41 -0400

    Making final product product util classes final

--- test/util/fixtures/FinalProductProductServiceFixtures.java
+++ test/util/fixtures/FinalProductProductServiceFixtures.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.LongStream;
 
-public class FinalProductProductServiceFixtures {
+public final class FinalProductProductServiceFixtures {
   public static final Long FAKEID = 10L;
 
   public static List<FinalProductProduct> buildFinalProductProducts(Integer size) {
